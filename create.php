67e5f6b69eff6e4f81638a95f66ee8f7506962fb
raphyrenu <raphyboy159@gmail.com>
2024-03-12 15:07:16 +0200

    first

---
+++ create.php
@@ -0,0 +1,26 @@
+<?php
+include 'connection.php';
+if (isset($_POST['submit'])){
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$gender=$_POST['gender'];
+$sql = "INSERT INTO users(fname,lname,email,password,gender) VALUES ('$fname','$lname','$email','$password','$gender')";
+
+$result = $conn->query($sql);
+if ($result == true) {
+    echo 'New record created raphy';
+}
+else {
+    echo 'Error bro correct please:'.$sql.'<br>'.$conn->error;
+}
+$conn->close();
+}
+
+
+?>
+<html>
+    <a class="btn btn-info" href="signup.html"><br><br>back</a>
+    <a class="btn btn-info" href="read.php"><br><br>view record from database</a>
+</html>
---
+++ read.php
@@ -0,0 +1,45 @@
+<?php
+include 'connection.php';
+$sql = 'SELECT * FROM users';
+$result = $conn->query($sql);
+?>
+<html>
+<head>
+<title>View Page</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <h2>users</h2>
+<table class="table">
+<thead>
+<tr>
+<th>ID</th>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Email</th>
+<th>Gender</th>
+<th>Action</th>
+<th>Action</th>
+</tr>
+</thead>
+<tbody> 
+<?php
+if ($result->num_rows > 0) {
+while ($row = $result->fetch_assoc()) {
+ ?>
+<tr>
+<td><?php echo $row['id']; ?></td>
+<td><?php echo $row['fname']; ?></td>
+<td><?php echo $row['lname']; ?></td>
+<td><?php echo $row['email']; ?></td>
+<td><?php echo $row['gender']; ?></td>
+
+<td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+</tr> 
+<?php }
+}
+?> 
+ </tbody></table></div></body></html>
+
+
